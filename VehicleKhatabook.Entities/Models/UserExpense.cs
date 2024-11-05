78d00f02e9b360dd702e4abd2d1d0115d54d7db1
mukesh271 <choudhary.mukesh271@gmail.com>
2024-11-05 15:50:59 +0530

    UserExpense added

---
+++ VehicleKhatabook.Entities/Models/UserExpense.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VehicleKhatabook.Entities.Models
+{
+    public class UserExpense : EntityBase
+    {
+        [Key]
+        public int ExpenseID { get; set; }
+
+        [Required]
+        public int ExpenseCategoryID { get; set; }
+
+        public decimal ExpenseAmount { get; set; }
+        public DateTime ExpenseDate { get; set; }
+
+        public string ExpenseDescription { get; set; }
+        public Guid UserID { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [ForeignKey("ExpenseCategoryID")]
+        public ExpenseCategory ExpenseCategory { get; set; }
+
+        [ForeignKey("UserID")]
+        public User user { get; set; }
+    }
+}
