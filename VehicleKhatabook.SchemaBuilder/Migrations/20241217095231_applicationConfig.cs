42c781847c4435cb6dde2994a343ad02a13d35b7
ankursharma182 <ankur.sharmaa@thecompetenza.com>
2024-12-17 16:01:53 +0530

    applicationconfiguration-Frontend and Backend

---
+++ VehicleKhatabook.SchemaBuilder/Migrations/20241217095231_applicationConfig.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleKhatabook.SchemaBuilder.Migrations
+{
+    /// <inheritdoc />
+    public partial class applicationConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SubscriptionTrialPeriodDays",
+                table: "ApplicationConfigurations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SubscriptionRenewalReminderDaysBefore",
+                table: "ApplicationConfigurations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SubscriptionIsRenewable",
+                table: "ApplicationConfigurations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SubscriptionDurationDays",
+                table: "ApplicationConfigurations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SubscriptionAmount",
+                table: "ApplicationConfigurations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentGatewayStatus",
+                table: "ApplicationConfigurations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IsActive",
+                table: "ApplicationConfigurations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SubscriptionTrialPeriodDays",
+                table: "ApplicationConfigurations",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubscriptionRenewalReminderDaysBefore",
+                table: "ApplicationConfigurations",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "SubscriptionIsRenewable",
+                table: "ApplicationConfigurations",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubscriptionDurationDays",
+                table: "ApplicationConfigurations",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SubscriptionAmount",
+                table: "ApplicationConfigurations",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "PaymentGatewayStatus",
+                table: "ApplicationConfigurations",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "ApplicationConfigurations",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
